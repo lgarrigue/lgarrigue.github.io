b0eb1fc06c3a5c8088f39e2404cddf85619c99f7
lgarrigue <louis.garrigue@gmail.com>
2022-10-28 18:59:48 +0200

    commit

--- misc/tex_macros.js
+++ misc/tex_macros.js
@@ -8,7 +8,7 @@ window.MathJax = {
       ps: ["{\\left< #1 \\right>}",1],
       myinf: ["{\\underset{\\substack{#1}}{\\text{inf}}\\quad}",1],
       f: ["{\\frac{#1}{#2}}",2],
-      nor: ["{\\left|  \\left| #1 \\right| \\right|_{#2}}",2],
+      nor: ["{\\left| \\left| #1 \\right| \\right|_{#2}}",2],
     }
   }
 };
