6a8ad1e998e109a47ce49f82504aa981f0cedbff
lgarrigue <louis.garrigue@gmail.com>
2024-02-27 17:10:08 +0100

    commit

---
+++ misc/tex_macros.js
@@ -0,0 +1,25 @@
+window.MathJax = {
+	tex: {
+		tags: 'ams',
+		macros: {
+			R: "{\\mathbb{R}}",
+			N: "{\\mathbb{N}}",
+			C: "{\\mathbb{C}}",
+			bbV: "{\\mathbb{V}}",
+			bbW: "{\\mathbb{W}}",
+			cR: "{\\mathcal{R}}",
+			cH: "{\\mathcal{H}}",
+			ep: "{\\varepsilon}",
+			d: "{{\\rm d}}\\def\\1{{\\mathds{1}}}",
+			pa: ["{\\left( #1 \\right)}",1],
+			ab: ["{\\left| #1 \\right|}",1],
+			ps: ["{\\left< #1 \\right>}",1],
+			mat: ["{\\begin{pmatrix} #1 \\end{pmatrix}}",1],
+			ind: ["{_{\\text{#1}}}",1],
+			myinf: ["{\\underset{\\substack{#1}}{\\text{inf}}\\quad}",1],
+			bibref: ["{\\hspace{-2cm}\\tiny{\\text{#1}}}",1],
+			f: ["{\\frac{#1}{#2}}",2],
+			nor: ["{\\left| \\left| #1 \\right| \\right|_{#2}}",2],
+		}
+	}
+};
